771deb488565cc8111303c3359297d40be34753e
Nicolas Chaput <6257251@cegepmontpetit.ca>
2025-05-09 18:18:11 -0400

    PROGRES : Test Delete

--- src/test/java/org/kickmyb/server/ServiceTaskTests.java
+++ src/test/java/org/kickmyb/server/ServiceTaskTests.java
@@ -18,6 +18,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Fail.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -42,7 +43,7 @@ class ServiceTaskTests {
     private ServiceTask serviceTask;
 
     @Test
-    void testDeleteTask() throws ServiceTask.Empty, ServiceTask.TooShort, ServiceTask.Existing, ServiceTask.NotOwner {
+    void testDeleteTaskIdCorrect() throws ServiceTask.Empty, ServiceTask.TooShort, ServiceTask.Existing, ServiceTask.NotOwner {
         MUser u = new MUser();
         u.username = "M. Test";
         u.password = passwordEncoder.encode("Passw0rd!");
@@ -58,11 +59,77 @@ class ServiceTaskTests {
             fail("La tâche n'a pas été ajoutée.");
         }
 
+        u = userRepository.findById(u.id).get();
         MTask task = u.tasks.getFirst();
 
         serviceTask.delete(task.id, u);
 
         assertEquals(0, serviceTask.home(u.id).size());
+    }
+
+    @Test
+    void testDeleteTaskIdIncorrect() throws ServiceTask.Empty, ServiceTask.TooShort, ServiceTask.Existing, ServiceTask.NotOwner {
+        MUser u = new MUser();
+        u.username = "M. Test";
+        u.password = passwordEncoder.encode("Passw0rd!");
+        userRepository.saveAndFlush(u);
+
+        AddTaskRequest atr = new AddTaskRequest();
+        atr.name = "Tâche de test";
+        atr.deadline = Date.from(new Date().toInstant().plusSeconds(3600));
+
+        serviceTask.addOne(atr, u);
+
+        if (serviceTask.home(u.id).size() != 1) {
+            fail("La tâche n'a pas été ajoutée.");
+        }
+
+        u = userRepository.findById(u.id).get();
+
+        try {
+            serviceTask.delete(2, u);
+            fail("Aurait du lancer une NoSuchElementException");
+        } catch (Exception e) {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
+    }
+
+    @Test
+    void testDeleteControleAcces() throws ServiceTask.Empty, ServiceTask.TooShort, ServiceTask.Existing, ServiceTask.NotOwner {
+        MUser u = new MUser();
+        u.username = "Alice";
+        u.password = passwordEncoder.encode("Passw0rd!");
+        userRepository.saveAndFlush(u);
+
+        MUser u2 = new MUser();
+        u2.username = "Bob";
+        u2.password = passwordEncoder.encode("Passw0rd!");
+        userRepository.saveAndFlush(u2);
+
+        AddTaskRequest atr = new AddTaskRequest();
+        atr.name = "Tâche de test";
+        atr.deadline = Date.from(new Date().toInstant().plusSeconds(3600));
+
+        serviceTask.addOne(atr, u);
+        serviceTask.addOne(atr, u2);
+
+        if (serviceTask.home(u.id).size() != 1) {
+            fail("La tâche n'a pas été ajoutée.");
+        }
+        if (serviceTask.home(u2.id).size() != 1) {
+            fail("La tâche n'a pas été ajoutée.");
+        }
+
+        u = userRepository.findById(u.id).get();
+        u2 = userRepository.findById(u2.id).get();
+        MTask task = u.tasks.getFirst();
+
+        try {
+            serviceTask.delete(task.id, u2);
+            fail("Aurait du lancer une NoSuchElementException");
+        } catch (Exception e) {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
     }
 
     @Test
